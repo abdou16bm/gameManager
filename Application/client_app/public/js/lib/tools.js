0a683e788269b612678d901f38f8515fc358e1ea
Ghano <ghanoatm95@gmail.com>
2024-10-23 13:20:49 +0100

    add filter product

--- Application/client_app/public/js/lib/tools.js
+++ Application/client_app/public/js/lib/tools.js
@@ -1,28 +1,88 @@
-let catBlock = document.querySelectorAll('.catBlock');
-let btnHideContent = document.querySelector('#btnHideContent');
-let tab_content = document.querySelector('.tab_content')
+const catBlockList = document.querySelectorAll('.catBlock');
+const btnHideContent = document.querySelector('#btnHideContent');
+const tab_content = document.querySelector('.tab_content');
+const searchBlock = document.querySelector("#searchBlock");
+const myInputSearch = document.querySelector("#myInputSearch");
+let filterCat = null
 
+if (catBlockList && btnHideContent) {
 
-if (catBlock != null && typeof catBlock != 'undefined' && btnHideContent != null && typeof btnHideContent != 'undefined')
-{
+  catBlockList.forEach(catBlock => {
+    
+    const catId = catBlock.getAttribute("catId");
+    catBlock.removeAttribute("catId");
 
-    for (let i=0;i<catBlock.length; i++)
-    {
-        catBlock[i].addEventListener('click', function (){
-            tab_content.classList.remove('option_hide')
-            btnHideContent.classList.remove("d-none")
-        })
+    catBlock.addEventListener('click', function (){
+
+      tab_content.classList.remove('option_hide');
+      btnHideContent.style.display = "inline";
+        
+      OpenFilterCat(catId);
+      
+    })
+
+  });
+
+ 
+  btnHideContent.addEventListener('click',function (){
+      
+    /* tab_content.classList.contains("option_hide") != true ? tab_content.classList.add('option_hide') : location.replace("/") */
+    if (tab_content.classList.contains("option_hide") != true) {
+
+      tab_content.classList.add('option_hide');
+      btnHideContent.style.display = "none";
+
+      hideFilterCat();
+      
     }
 
-    btnHideContent.addEventListener('click',function (){
+  })
+  
+}
+
+
+
+
+function OpenFilterCat(catId) {
+  
+  if (catId) {
+    
+    filterCat = catId;
+    const productsTab = document.querySelector("#productsTab-" + filterCat);
+    const productBlock_list = productsTab.querySelectorAll("#productBlock");
+
+    myInputSearch.oninput = () => {
+
+      const value = myInputSearch.value;
+      if (value) {
+          
+        productBlock_list.forEach(product => {
         
-      /* tab_content.classList.contains("option_hide") != true ? tab_content.classList.add('option_hide') : location.replace("/") */
-      if (tab_content.classList.contains("option_hide") != true) {
+          const name = product.getAttribute("productData-search");
+          name.toLocaleLowerCase().includes(value.toLocaleLowerCase()) ? product.style.display = "block" : product.style.display = "none"
+    
+        });
 
-        tab_content.classList.add('option_hide');
-        btnHideContent.classList.add("d-none")
+      } else productBlock_list.forEach(product => product.style.display = "block");
 
-      }
+    }
 
-    })
+    searchBlock ? searchBlock.style.display = "flex" : false;
+    myInputSearch.focus()
+
+  }
+
+}
+
+function hideFilterCat() {
+  
+  const productsTab = document.querySelector("#productsTab-" + filterCat);
+  const productBlock_list = productsTab.querySelectorAll("#productBlock");
+
+  myInputSearch ? myInputSearch.value = "" : false;
+  productBlock_list.forEach(product => product.style.display = "block");
+
+  filterCat = null;
+  searchBlock ? searchBlock.style.display = "none" : false;
+
 }
